041181ede1a026c2df827d7cdfbb09083991e39e
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-10-19 23:25:37 +0600

    firebase config updated

--- src/Firebase/firebase.initialize.js
+++ src/Firebase/firebase.initialize.js
@@ -6,3 +6,14 @@ const initializeAuthentication = () => {
 } 
 
 export default initializeAuthentication;
+
+/*
+const firebaseConfig = {
+  apiKey: "AIzaSyBaFbdErGZF687PeYER0o-ylfWGqDraCDc",
+  authDomain: "ibn-sina-new.firebaseapp.com",
+  projectId: "ibn-sina-new",
+  storageBucket: "ibn-sina-new.appspot.com",
+  messagingSenderId: "129641788559",
+  appId: "1:129641788559:web:173766b277703fbda3acfa"
+};
+*/
